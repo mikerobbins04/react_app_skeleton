a730474ac64cce0f7512816672dd849cee4cbb2c
mikerobbins04 <mikerobbins04@yahoo.com>
2023-03-19 13:10:41 -0400

    First build commit

---
+++ build/precache-manifest.11cf52ada03d092e88edb18169ef348b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1533b94fdafad09e571b",
+    "url": "/static/css/main.c71a8b86.chunk.css"
+  },
+  {
+    "revision": "1533b94fdafad09e571b",
+    "url": "/static/js/main.1533b94f.chunk.js"
+  },
+  {
+    "revision": "1af2bac16cf7e73aa3f5",
+    "url": "/static/js/1.1af2bac1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9d0c29fabc020e18485696ccd65bfd2",
+    "url": "/index.html"
+  }
+];
